4b07db4712528f33e03cd240f07b51d3772e1e4b
melloware <mellowaredev@gmail.com>
2023-11-07 15:12:00 -0500

    Gatsby site

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/footer.tsx
@@ -0,0 +1,38 @@
+/** @jsx jsx */
+import { jsx, Link } from "theme-ui";
+import useSiteMetadata from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-site-metadata";
+
+const Footer = () => {
+  const { siteTitle } = useSiteMetadata();
+
+  return (
+    <footer
+      sx={{
+        boxSizing: `border-box`,
+        display: `flex`,
+        justifyContent: `space-between`,
+        mt: [3],
+        color: `secondary`,
+        a: {
+          variant: `links.secondary`,
+        },
+        flexDirection: [`column`, `column`, `row`],
+        variant: `dividers.top`,
+      }}
+    >
+      <div>
+        &copy; {new Date().getFullYear()} by {siteTitle}. All rights reserved.
+      </div>
+      <div>
+        <Link aria-label="Stack Exchange reputation" href="https://stackoverflow.com/users/502366/melloware">
+          <img src="https://img.shields.io/stackexchange/stackoverflow/r/502366?style=for-the-badge"></img>
+        </Link>
+        <Link aria-label="GitHub followers" href="https://github.com/melloware" style={{ marginLeft: "0.5rem" }}>
+          <img src="https://img.shields.io/github/followers/melloware?style=for-the-badge"></img>
+        </Link>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/header-title.tsx
@@ -0,0 +1,19 @@
+/** @jsx jsx */
+import { Link } from "gatsby";
+import { jsx } from "theme-ui";
+import replaceSlashes from "@lekoarts/gatsby-theme-minimal-blog/src/utils/replaceSlashes";
+import useSiteMetadata from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-site-metadata";
+import useMinimalBlogConfig from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-minimal-blog-config";
+
+const HeaderTitle = () => {
+  const { siteTitle } = useSiteMetadata();
+  const { basePath } = useMinimalBlogConfig();
+
+  return (
+    <Link to={replaceSlashes(`/${basePath}`)} aria-label={`${siteTitle} - Back to home`} sx={{ color: `heading`, textDecoration: `none` }}>
+      <img alt="Melloware Home" src={`/svg/logo-no-background.svg`} height="120" width="365"></img>
+    </Link>
+  );
+};
+
+export default HeaderTitle;
