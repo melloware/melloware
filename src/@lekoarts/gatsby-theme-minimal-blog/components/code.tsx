3c2dad7032396db1d203d7f76e010affbaaa95f7
melloware <mellowaredev@gmail.com>
2023-12-19 15:49:11 -0500

    Jakarta EE10

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/code.tsx
@@ -0,0 +1,10 @@
+import Code from "@lekoarts/gatsby-theme-minimal-blog/src/components/code";
+import { Prism } from "prism-react-renderer";
+
+// @ts-ignore
+(typeof global !== "undefined" ? global : window).Prism = Prism;
+// Add new languages by `require()`-ing it.
+// See https://github.com/PrismJS/prism/tree/master/components for a full list.
+require("prismjs/components/prism-java");
+
+export default Code;
